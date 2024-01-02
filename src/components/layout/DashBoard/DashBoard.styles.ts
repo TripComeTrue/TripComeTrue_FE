ef4d8c58210a0b94ba5ec061dc7b26b83778b0f7
JiHongkyu <hongkyu9941@gmail.com>
2024-01-02 17:59:35 +0900

    Refactor: msw버전 변경

--- src/components/layout/DashBoard/DashBoard.styles.ts
+++ src/components/layout/DashBoard/DashBoard.styles.ts
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 const Container = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
 
   position: relative;
   overflow-x: hidden;
