6c8e37dfc12194d475930d7b2bf4e77551fb98e4
JiHongkyu <hongkyu9941@gmail.com>
2024-01-04 21:15:46 +0900

    style: Text 공통 컴포넌트 생성 (#13)

---
+++ src/components/common/Text/Text.styles.ts
@@ -0,0 +1,28 @@
+import styled, { css } from 'styled-components';
+
+const textColor = {
+  white: css`
+    color: ${({ theme }) => theme.brand.white};
+  `,
+  black: css`
+    color: ${({ theme }) => theme.text.black};
+  `,
+  gray: css`
+    color: ${({ theme }) => theme.text.gray};
+  `,
+  primary: css`
+    color: ${({ theme }) => theme.brand.primary};
+  `,
+};
+
+export const Text = styled.span<{
+  fontSize?: string;
+  fontWeight?: number;
+  color?: 'white' | 'black' | 'gray' | 'primary';
+}>`
+  font-size: ${({ fontSize }) => fontSize};
+  font-weight: ${({ fontWeight }) => fontWeight};
+  ${({ color }) => color && textColor[color]}
+`;
+
+export default Text;
--- src/components/common/Title/Title.styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const Title = styled.h2`
-  font-size: ${(props) => props.theme.fontSizes.lg};
-  font-weight: ${(props) => props.theme.fontWeights.bold};
-  color: ${(props) => props.theme.text.black};
-`;
-
-export default Title;
