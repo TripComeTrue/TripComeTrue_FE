62ad6a0c74207880a549db4f27536150a06470d3
skyeome <tjru7230@naver.com>
2024-01-08 17:37:51 +0900

    feat: 아이디/비번찾기 임시로 숨기기

--- src/components/auth/SignInLinks/SignInLinks.tsx
+++ src/components/auth/SignInLinks/SignInLinks.tsx
@@ -3,12 +3,12 @@ import * as Styled from './SignInLinks.styles';
 function SignInLinks() {
   return (
     <Styled.SignInLinkWrap>
-      <Styled.SignInLinkItem>
+      {/* <Styled.SignInLinkItem>
         <Styled.SignInLink to="/auth/findemail">아이디 찾기</Styled.SignInLink>
       </Styled.SignInLinkItem>
       <Styled.SignInLinkItem>
         <Styled.SignInLink to="/auth/findpw">비밀번호 찾기</Styled.SignInLink>
-      </Styled.SignInLinkItem>
+      </Styled.SignInLinkItem> */}
       <Styled.SignInLinkItem>
         <Styled.SignInLink to="/auth/agree">회원가입</Styled.SignInLink>
       </Styled.SignInLinkItem>
