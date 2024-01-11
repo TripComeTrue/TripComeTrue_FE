bad35efc822463b9c80d283b9dd17fdcb750ebfc
yuna <seacrab808@naver.com>
2024-01-11 15:02:23 +0900

    Feat: 주석 추가

--- src/components/common/Shorts/Shorts.tsx
+++ src/components/common/Shorts/Shorts.tsx
@@ -1,7 +1,12 @@
 import { Slide, ShortsProps } from './Shorts.types';
 import ShortsSwiper from './ShortsSwiper';
-
 import * as Styled from './Shorts.style';
+
+/**
+ * 쇼츠 공통 컴포넌트입니다.
+ * @param slides 쇼츠에 들어가야하는 쇼츠 제목(string), 쇼츠 대표 이미지(string), 쇼츠 북마크(number) 등이 들어있는 배열입니다.
+ * @param slidesPerView 한 화면에 출력되는 slide 갯수
+ */
 
 const Shorts: React.FC<ShortsProps> = ({ slides, slidesPerView = 2.1 }) => (
   <Styled.SwiperWrap
