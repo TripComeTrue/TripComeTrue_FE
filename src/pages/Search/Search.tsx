6032f503b2345069e81baec1555d6ffde27ff513
seacrab808 <seacrab808@naver.com>
2024-01-15 01:35:59 +0900

    Design: 검색/탭바 스타일링

--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -1,6 +1,90 @@
-import React, { useEffect, useState } from 'react';
+import React, { ButtonHTMLAttributes, useEffect, useState } from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
+import { IoIosArrowBack } from 'react-icons/io';
+import { IoSearchOutline } from 'react-icons/io5';
+import styled from 'styled-components';
 import SearchResult from './SearchResult';
+
+interface TabButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  isSelected: boolean;
+}
+
+const SearchTop = styled.div`
+  width: 100%;
+
+  display: grid;
+  grid-template-columns: 0.9fr 1.1fr;
+
+  padding: 0.56rem 1.25rem 0.56rem 1rem;
+
+  font-size: ${({ theme }) => theme.fontSizes.md};
+`;
+
+const SearchForm = styled.form`
+  position: relative;
+  margin: 1rem;
+
+  button {
+    position: absolute;
+    top: 15%;
+    right: 1.2rem;
+    color: ${({ theme }) => theme.brand.primary};
+  }
+`;
+
+const SearchInput = styled.input`
+  padding: 0.6rem 3rem 0.6rem 1.5rem;
+  width: 100%;
+
+  box-sizing: border-box;
+  background-color: #2f2f2f;
+  border-radius: 1.875rem;
+  text-decoration: none;
+  border: none;
+  outline: none;
+  color: ${({ theme }) => theme.brand.white};
+  font-size: ${({ theme }) => theme.fontSizes.sm};
+
+  &:focus {
+    border: none;
+    outline: none;
+  }
+
+  &::placeholder {
+    color: ${({ theme }) => theme.brand.white};
+    font-size: ${({ theme }) => theme.fontSizes.sm};
+  }
+`;
+
+const TabContainer = styled.div`
+  margin: 0 1rem 1rem;
+  display: flex;
+  justify-content: flex-start;
+`;
+
+const TabButton = styled.button<TabButtonProps>`
+  position: relative;
+  padding: 0.5rem 0.7rem;
+  border: none;
+  background-color: transparent;
+  font-size: ${({ theme }) => theme.fontSizes.sm};
+  font-weight: ${({ isSelected }) => (isSelected ? 'bold' : 'normal')};
+  color: ${({ isSelected }) => (isSelected ? '#373737' : '#626262')};
+
+  &::after {
+    content: '';
+    position: absolute;
+    left: 50%;
+    bottom: 0;
+    height: 0.125rem;
+    background-color: ${({ theme }) => theme.brand.primary};
+    transition:
+      width 0.3s,
+      transform 0.3s;
+    transform: translateX(-50%);
+    width: ${({ isSelected }) => (isSelected ? '80%' : '0')};
+  }
+`;
 
 const Search: React.FC = () => {
   const [query, setQuery] = useState('');
@@ -26,53 +110,78 @@ const Search: React.FC = () => {
   };
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault(); // 기본 제출 동작 방지
+    e.preventDefault();
     handleSearch();
   };
 
   if (searchParams.get('query') === null) {
     return (
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="query"
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-        />
-        <button type="submit">검색</button>
-      </form>
+      <>
+        <SearchTop>
+          <IoIosArrowBack style={{ fontSize: 20, marginRight: 8 }} />
+          검색
+        </SearchTop>
+        <SearchForm onSubmit={handleSubmit}>
+          <SearchInput
+            type="text"
+            name="query"
+            value={query}
+            placeholder="도시, 여행지, 크리에이터 검색하기"
+            onChange={(e) => setQuery(e.target.value)}
+          />
+          <button type="submit" aria-label="검색">
+            <IoSearchOutline />
+          </button>
+        </SearchForm>
+      </>
     );
   }
 
   if (searchParams.has('tab') && searchParams.has('query')) {
     return (
-      <div>
-        <form onSubmit={handleSubmit}>
-          <input
+      <>
+        <SearchTop>
+          <IoIosArrowBack style={{ fontSize: 20, marginRight: 8 }} />
+          검색
+        </SearchTop>
+        <SearchForm onSubmit={handleSubmit}>
+          <SearchInput
             type="text"
             name="query"
             value={query}
+            placeholder="도시, 여행지, 크리에이터 검색하기"
             onChange={(e) => setQuery(e.target.value)}
           />
-          <button type="submit">검색</button>
-        </form>
-        <div>
-          <button type="button" onClick={() => handleTabChange('all')}>
+          <button type="submit" aria-label="검색">
+            <IoSearchOutline />
+          </button>
+        </SearchForm>
+
+        <TabContainer>
+          <TabButton
+            isSelected={tab === 'all'}
+            onClick={() => handleTabChange('all')}>
             전체
-          </button>
-          <button type="button" onClick={() => handleTabChange('city')}>
+          </TabButton>
+          <TabButton
+            isSelected={tab === 'city'}
+            onClick={() => handleTabChange('city')}>
             도시
-          </button>
-          <button type="button" onClick={() => handleTabChange('spot')}>
+          </TabButton>
+          <TabButton
+            isSelected={tab === 'spot'}
+            onClick={() => handleTabChange('spot')}>
             여행지
-          </button>
-          <button type="button" onClick={() => handleTabChange('creator')}>
+          </TabButton>
+          <TabButton
+            isSelected={tab === 'creator'}
+            onClick={() => handleTabChange('creator')}>
             크리에이터
-          </button>
-        </div>
+          </TabButton>
+        </TabContainer>
 
         <SearchResult tab={tab} />
-      </div>
+      </>
     );
   }
 
