ea583c010248be12aeb8335654dfc3581426c717
mysdpy30s <mysdpy30s@gmail.com>
2024-01-17 18:40:32 +0900

    Fix: 미작업 파일 삭제

--- src/components/Trip/TripPlan/TripPlanPosting/PlaceListModal/PlaceListModal.styles.ts
+++
@@ -1,9 +0,0 @@
-// import styled from 'styled-components';
-
-// export const ShowPlacesContainer = styled.div`
-//   display: flex;
-//   flex-direction: column;
-
-//   padding: 0.2rem 0.5rem;
-//   gap: 1rem;
-// `;
