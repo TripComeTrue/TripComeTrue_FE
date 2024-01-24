cdb7b33af55cd6a5f512c234adfe38d846b62c6e
yuna <seacrab808@naver.com>
2024-01-25 03:19:01 +0900

    Feat: search-non페이지에 api연결

--- src/components/Search/SearchNon/SearchNon.tsx
+++ src/components/Search/SearchNon/SearchNon.tsx
@@ -1,43 +1,40 @@
+import { useEffect, useState } from 'react';
 import { Shorts, Spots } from '@/components/common';
 import SearchKeyword from '../SearchKeyword/SearchKeyword';
-import domestic1 from '/domestic3.jpg';
-import { HomeMDDataType, SwiperProps } from './SearchNon.types';
 import * as Styled from './SearchNon.styles';
+import { HomeShortsList, HomeTopCity } from '@/apis/home';
 
 const SearchNon = () => {
-  const slideShorts: SwiperProps = {
-    전체: [
-      { img: domestic1, title: '해외같은 제주 풀빌라', bookmark: 234 },
-      { img: domestic1, title: '스페인의 길거리', bookmark: 125 },
-      { img: domestic1, title: '서울 이색 데이트 추천!', bookmark: 88 },
-      { img: domestic1, title: '디즈니 성 몽생미셸', bookmark: 632 },
-      { img: domestic1, title: '추운 겨울의 글램핑', bookmark: 231 },
-      { img: domestic1, title: '일본 온천 여행', bookmark: 115 },
-    ],
-  };
+  const [shortsData, setShortsData] = useState([]);
+  const [hotReview, setHotReview] = useState([]);
 
-  const HomeMDdata: HomeMDDataType = {
-    france: [
-      {
-        bookmark: 123,
-        postTitle: '치앙마이 다녀왔어요!',
-        postImg: domestic1,
-        reviews: 23,
-      },
-      {
-        bookmark: 231,
-        postTitle: '태국 치앙마이 가볼만한',
-        postImg: domestic1,
-        reviews: 23,
-      },
-      {
-        bookmark: 421,
-        postTitle: '치앙마이 한달살기',
-        postImg: domestic1,
-        reviews: 23,
-      },
-    ],
-  };
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await HomeShortsList('all');
+        setShortsData(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  console.log(shortsData);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await HomeTopCity('overseas');
+        setHotReview(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  console.log(hotReview);
 
   return (
     <div>
@@ -49,11 +46,11 @@ const SearchNon = () => {
       </Styled.SearchNonWrap>
 
       <Styled.SearchShorts>쇼츠</Styled.SearchShorts>
-      <Shorts slides={slideShorts.전체} slidesPerView={2.1} />
+      <Shorts slides={shortsData} slidesPerView={2.1} />
 
       <Styled.SearchReview>여행 후기</Styled.SearchReview>
       <Spots
-        creator={HomeMDdata.france}
+        creator={hotReview}
         slidesPerView={2.75}
         sort="center"
         fontSize={10}
