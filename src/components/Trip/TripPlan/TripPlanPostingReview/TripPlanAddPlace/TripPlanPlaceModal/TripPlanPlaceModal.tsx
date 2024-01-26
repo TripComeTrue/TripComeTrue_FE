4c4eca72f3926be6972d58f4722680e675f589ff
mysdpy30s <mysdpy30s@gmail.com>
2024-01-26 15:58:32 +0900

    Feat: 여행 장소 선택 모달 코드 수정

--- src/components/Trip/TripPlan/TripPlanPostingReview/TripPlanAddPlace/TripPlanPlaceModal/TripPlanPlaceModal.tsx
+++ src/components/Trip/TripPlan/TripPlanPostingReview/TripPlanAddPlace/TripPlanPlaceModal/TripPlanPlaceModal.tsx
@@ -1,43 +1,27 @@
 import { useEffect, useState } from 'react';
-// import { useQuery } from '@tanstack/react-query';
+import { useQuery } from '@tanstack/react-query';
 import { Link } from 'react-router-dom';
 import { AiFillPlusCircle } from 'react-icons/ai';
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import * as Styled from './TripPlanPlaceModal.styles';
 import { Button, SubTitle } from '@/components/common';
 import { SelectedPlaceProps } from './TripPlanPlaceModal.types';
-// import { getTripPlaces } from '@/apis/trip-planandrecords';
+import { getTripPlaces } from '@/apis/trip-planandrecords';
+import { Place } from '@/@types/trip-alldata.types';
 
 const TripPlanPlaceModal: React.FC<SelectedPlaceProps> = ({
   selectedPlace,
   onPlaceSelection,
   onCloseModal,
+  countryName,
+  cityName,
 }: SelectedPlaceProps) => {
-  // const { data: tripPlacesData } = useQuery({
-  //   queryKey: ['TripPlaces'],
-  //   queryFn: () => getTripPlaces(countryName, cityName),
-  // });
+  const { data: tripPlacesData } = useQuery({
+    queryKey: ['TripPlaces', countryName, cityName],
+    queryFn: () => getTripPlaces(countryName, cityName),
+  });
 
   const [selectedPlaceInModal, setSelectedPlaceInModal] = useState<string>('');
-
-  const placesForSelectedCity = [
-    {
-      name: '에펠탑',
-      address: 'Champ de Mars, 5 Avenue Anatole France, Paris 7',
-    },
-    {
-      name: '루브르 박물관',
-      address: 'France, 75001 Paris',
-    },
-    {
-      name: '에투알 개선문',
-      address: 'Pl. Charles de Gaulle, 75008 Paris, France',
-    },
-    {
-      name: '오르세 미술관',
-      address: "Esplanade Valéry Giscard d'Estaing 75007 Paris",
-    },
-  ];
 
   const selectPlace = (place: string) => {
     setSelectedPlaceInModal(place);
@@ -64,13 +48,13 @@ const TripPlanPlaceModal: React.FC<SelectedPlaceProps> = ({
       </Link>
       <SubTitle margin="1rem 0">추천 방문장소</SubTitle>
       <Styled.ShowPlacesContainer>
-        {placesForSelectedCity.map((place) => (
+        {tripPlacesData.map((place: Place) => (
           <Styled.EachPlace
-            key={place.name}
+            key={place.placeId}
             onClick={() => selectPlace(place.name)}
             selected={selectedPlace.includes(place.name)}>
-            {/* <img src={image} alt="city" /> */}
             {place.name}
+            {place.address}
             <CheckCircleIcon className="checked" fill="#b4f34c" />
           </Styled.EachPlace>
         ))}
