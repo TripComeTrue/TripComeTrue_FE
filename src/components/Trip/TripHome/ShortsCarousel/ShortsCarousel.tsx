3afca00dc6fc8febd7c4b8d25b5aabfabc23769e
JiHongkyu <hongkyu9941@gmail.com>
2024-01-28 22:12:03 +0900

    refactor: 여행 메인 페이지 쇼츠 수정

--- src/components/Trip/TripHome/ShortsCarousel/ShortsCarousel.tsx
+++ src/components/Trip/TripHome/ShortsCarousel/ShortsCarousel.tsx
@@ -2,30 +2,15 @@ import ShortCard from '../ShortCard/ShortCard';
 import * as Styled from './ShortsCarousel.styles';
 import 'swiper/swiper-bundle.css';
 import { ShortsCarouselProps } from './ShortsCarousel.types';
-import useModal from '@/hooks/common/useModal';
-import { ShortsModal } from '@/components/common';
-import videoKey from '@/utils/videoKey';
 
 const ShortsCarousel = ({ shortsData }: ShortsCarouselProps) => {
-  const { open, handleOpen, handleClose } = useModal();
+  console.log(shortsData);
 
   return (
     <Styled.Container spaceBetween={8} slidesPerView={2.3}>
       {shortsData?.map((shortData) => (
-        <Styled.Slide key={shortData.videoId} onClick={handleOpen}>
+        <Styled.Slide key={shortData.videoId}>
           <ShortCard shortData={shortData} />
-          {open && (
-            <ShortsModal
-              open={open}
-              onClose={handleClose}
-              videoId={videoKey(shortData.videoUrl)}
-              profileImageUrl={shortData.member.profileImage}
-              memberName={shortData.member.nickname}
-              tripRecordId={shortData.tripRecordId}
-              tripRecordTitle={shortData.tripRecordTitle}
-              memberId={shortData.memberId}
-            />
-          )}
         </Styled.Slide>
       ))}
     </Styled.Container>
